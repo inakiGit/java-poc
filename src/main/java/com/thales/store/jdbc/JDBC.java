e36e320dff74fdff3182f483cf6cf75416b8e382
iaguilar <inaki.aguilar@mythalesgroup.io>
2020-03-17 14:52:38 +0100

    DB connection OK

---
+++ src/main/java/com/thales/store/jdbc/JDBC.java
@@ -0,0 +1,24 @@
+package com.thales.store.jdbc;
+
+import java.sql.*;
+
+public class JDBC {
+
+    private static final String SELECT_QUERY = "SELECT * FROM users";
+
+    private static final String host = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String uName = "postgres";
+    private static final String uPass = "caudillo";
+
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = DriverManager.getConnection(host, uName, uPass);
+            if (conn != null) System.out.println("Connected");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SELECT_QUERY);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
