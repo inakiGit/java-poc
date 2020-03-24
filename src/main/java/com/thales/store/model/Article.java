63f8860fd9c46d7984a8f7b8eadc79e509693e58
iaguilar <inaki.aguilar@mythalesgroup.io>
2020-03-24 16:35:38 +0100

    lombok

--- src/main/java/com/thales/store/model/Article.java
+++ src/main/java/com/thales/store/model/Article.java
@@ -1,7 +1,10 @@
 package com.thales.store.model;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Article {
 
     private String name;
@@ -17,52 +20,5 @@ public class Article {
         this.label = label;
         this.price = price;
         this.stock = stock;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "name='" + name + '\'' +
-                ", label='" + label + '\'' +
-                ", price=" + price +
-                ", stock=" + stock +
-                '}';
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getLabel(), getPrice(), getStock());
     }
 }
