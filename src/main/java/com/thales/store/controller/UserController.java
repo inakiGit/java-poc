f06be9ecdd3bc22ff49a033a6929f95e9188c35e
iaguilar <inaki.aguilar@mythalesgroup.io>
2020-03-26 18:24:18 +0100

    Usercontroller finsOne findAll (tested with ToString)

--- src/main/java/com/thales/store/controller/UserController.java
+++ src/main/java/com/thales/store/controller/UserController.java
@@ -2,6 +2,7 @@ package com.thales.store.controller;
 
 import com.thales.store.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value="/users",method= RequestMethod.GET)
-    public String getUser(){
-        return userService.findOne();
+    @RequestMapping(value="/users/{userId}", method= RequestMethod.GET)
+    public String getOneById(@PathVariable("userId") int userId){
+        return userService.findOneById(userId);
+    }
+
+    @RequestMapping(value="/users", method= RequestMethod.GET)
+    public String getAllById(){
+        return userService.findAll();
     }
 }
--- src/main/java/com/thales/store/service/UserService.java
+++ src/main/java/com/thales/store/service/UserService.java
@@ -12,7 +12,11 @@ public class UserService {
 
     private static final String pass = "caudillo";
 
-    public String findOne(){
-        return new JdbcUserDAO(host, name, pass).findOneById(1).toString();
+    public String findOneById(int userId){
+        return new JdbcUserDAO(host, name, pass).findOneById(userId).toString();
+    }
+
+    public String findAll(){
+        return new JdbcUserDAO(host, name, pass).findAll().toString();
     }
 }
