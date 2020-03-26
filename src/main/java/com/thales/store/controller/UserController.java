71aba6cadc9101f0b79b1d299063b7815aa812da
iaguilar <inaki.aguilar@mythalesgroup.io>
2020-03-26 13:18:41 +0100

    bean printer

--- src/main/java/com/thales/store/controller/UserController.java
+++ src/main/java/com/thales/store/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.thales.store.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @RequestMapping(value="/users",method= RequestMethod.GET)
+    public String getUser(){
+
+        return "Hellooooo";
+    }
 
 }
